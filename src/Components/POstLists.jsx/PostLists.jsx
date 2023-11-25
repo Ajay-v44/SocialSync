59478b5a3a2f945ec72ca9a2e50a4d2ef7b4f1a2
Ajay v <vajay4834@gmail.com>
2023-11-25 11:27:36 +0530

    useEffect added

--- src/Components/POstLists.jsx/PostLists.jsx
+++ src/Components/POstLists.jsx/PostLists.jsx
@@ -1,24 +1,33 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import Posts from "../Posts/Posts";
 import { PostList } from "../Store/PostsListProvider";
 import WelcomeMessage from "../WelcomMEssage/WelcomeMessage";
 
 const PostLists = () => {
-  const { postList,addInitialPost } = useContext(PostList); 
-  const handleGetPostsClick =()=>{
-    fetch('https://dummyjson.com/posts')
-    .then(res => res.json())
-    .then(data=>{
-      addInitialPost(data.posts)
-    });
-                
-  }
+  const { postList, addInitialPost } = useContext(PostList);
+  useEffect(() => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPost(data.posts);
+      });
+  }, []);
+
+  const handleGetPostsClick = () => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPost(data.posts);
+      });
+  };
 
   return (
     <div>
-      {postList.length===0 && <WelcomeMessage onGetPostsClick={handleGetPostsClick}/>}
+      {postList.length === 0 && (
+        <WelcomeMessage onGetPostsClick={handleGetPostsClick} />
+      )}
       {postList.map((post) => (
-        <Posts key={post.id} post={post}/>
+        <Posts key={post.id} post={post} />
       ))}
     </div>
   );
--- src/Components/Store/PostsListProvider.jsx
+++ src/Components/Store/PostsListProvider.jsx
@@ -7,45 +7,42 @@ export const PostList = createContext({
 });
 
 const postListReducer = (currPostList, action) => {
-  let newPostilist=currPostList
-  if(action.type=='DELETE_POST'){
-    newPostilist=currPostList.filter(post=>post.id!==action.payload.postid);
-  }else if(action.type=="ADD_POST")
-  {
-    newPostilist=[action.payload,...currPostList]
-  }
-  else if(action.type=="ADD_InitialPOST"){
-    newPostilist=action.payload.posts
+  let newPostilist = currPostList;
+  if (action.type == "DELETE_POST") {
+    newPostilist = currPostList.filter(
+      (post) => post.id !== action.payload.postid
+    );
+  } else if (action.type == "ADD_POST") {
+    newPostilist = [action.payload, ...currPostList];
+  } else if (action.type == "ADD_InitialPOST") {
+    newPostilist = action.payload.posts;
   }
   return newPostilist;
 };
 
 const PostListProvider = ({ children }) => {
-  const [postList, dispatchPostList] = useReducer(
-    postListReducer,
-   []
-  );
-  const addPost = (uid,postit,posbody,reactions,tags) => {
+  const [postList, dispatchPostList] = useReducer(postListReducer, []);
+  const addPost = (uid, postit, posbody, reactions, tags) => {
     dispatchPostList({
-      type:"ADD_POST",
-      payload:  {
+      type: "ADD_POST",
+      payload: {
         id: Date.now(),
         title: postit,
         body: posbody,
-        reaction:reactions,
+        reactions: reactions,
         userId: uid,
-        tags:tags,
-      }
-    })
+        tags: tags,
+      },
+    });
   };
 
   const addInitialPost = (posts) => {
     dispatchPostList({
-      type:"ADD_InitialPOST",
-      payload:  {
-   posts
-      }
-    })
+      type: "ADD_InitialPOST",
+      payload: {
+        posts,
+      },
+    });
   };
   const deletePost = (postid) => {
     dispatchPostList({
@@ -57,7 +54,9 @@ const PostListProvider = ({ children }) => {
   };
 
   return (
-    <PostList.Provider value={{ postList, addPost,addInitialPost, deletePost }}>
+    <PostList.Provider
+      value={{ postList, addPost, addInitialPost, deletePost }}
+    >
       {children}
     </PostList.Provider>
   );
